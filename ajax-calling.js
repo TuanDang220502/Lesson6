ff06a185ba2651c292eaaa81621996b333b69dc0
TuanDang220502 <94059611+TuanDang220502@users.noreply.github.com>
2021-11-24 11:47:41 +0700

    Update ajax-calling.js

--- ajax-calling.js
+++ ajax-calling.js
@@ -1,7 +1,7 @@
 const callAPI = () => {
     console.log(123);
     $.ajax({
-        url: "https://619d074d68ebaa001753ce6a.mockapi.io/api/:endpoint",
+        url: "https://619d074d68ebaa001753ce6a.mockapi.io/api/products",
         method: "GET", // GET or POST or PUT or DELETE
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: result => {
